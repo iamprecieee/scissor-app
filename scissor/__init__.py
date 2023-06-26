f7f832e632021b407df0216c2327207e258facd4
Precious Iheanyi <emmypresh777@gmail.com>
2023-06-26 13:34:17 +0100

    Update __init__.py

--- scissor/__init__.py
+++ scissor/__init__.py
@@ -1,38 +1,32 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-import secrets, os
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_caching import Cache
 from flask_migrate import Migrate
 
+from .config import Config
+
 db = SQLAlchemy()
 migrate = Migrate()
-DB_NAME = "database.db"
 
 
-def create_app(config_name=None):
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
-    app.config['JWT_SECRET_KEY'] = secrets.token_urlsafe(16)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    
-
-    app.config['SERVER_NAME'] = os.getenv('SERVER_NAME', 'scissor-app.onrender.com')
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    cache = Cache(app, config={
+        'CACHE_TYPE': app.config['CACHE_TYPE'], 
+        'CACHE_REDIS_URL': app.config['CACHE_REDIS_URL']
+    })
+    app.cache = cache
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    REDIS_URL = os.getenv("REDIS_URL")
-    
-    global cache
-    cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': REDIS_URL})
-    
-    global limiter
-    limiter = Limiter(key_func=get_remote_address, app=app, storage_uri=REDIS_URL)
+    limiter = Limiter(key_func=get_remote_address, app=app, storage_uri=app.config['REDIS_URL'])
+    app.limiter = limiter
 
 
     from .views import views
@@ -43,8 +37,6 @@ def create_app(config_name=None):
 
     from .models import User
 
-    with app.app_context():
-        db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -53,5 +45,8 @@ def create_app(config_name=None):
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    with app.app_context():
+        db.create_all()
 
     return app
