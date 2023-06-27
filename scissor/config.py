ed067fb18296f5e418bc96759a49b9ba0eb9d3ac
Precious Iheanyi <emmypresh777@gmail.com>
2023-06-27 13:18:26 +0100

    Update config.py

--- scissor/config.py
+++ scissor/config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///database.db')
-    SERVER_NAME = os.getenv('SERVER_NAME', 'scssr.tech')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SERVER_NAME = os.getenv("SERVER_NAME", 'scssr.tech')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_URL = os.getenv("REDIS_URL")
     CACHE_TYPE = 'redis'
