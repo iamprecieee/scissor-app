fb240b605782b0c831792d18bf86a740fa164c59
Precious Iheanyi <emmypresh777@gmail.com>
2023-06-27 10:05:06 +0100

    Update config.py

--- scissor/config.py
+++ scissor/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///database.db')
-    SERVER_NAME = os.getenv('SERVER_NAME', 'scissor-app.onrender.com')
+    SERVER_NAME = os.getenv('SERVER_NAME')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_URL = os.getenv("REDIS_URL")
     CACHE_TYPE = 'redis'
