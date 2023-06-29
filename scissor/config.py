d2f4d7d64590b58635c3483fab923f7a469d56cd
Precious Iheanyi <emmypresh777@gmail.com>
2023-06-29 20:51:22 +0100

    Update config.py

--- scissor/config.py
+++ scissor/config.py
@@ -8,6 +8,12 @@ class Config:
     REDIS_URL = os.getenv("REDIS_URL")
     CACHE_TYPE = 'redis'
     CACHE_REDIS_URL = REDIS_URL
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
 
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
